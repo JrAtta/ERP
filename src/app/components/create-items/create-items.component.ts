5bd18620d9476e7a8c1b148e0924d8d5da1c9734
heka <Aee23530@gmail.com>
2025-03-12 01:38:31 +0200

    work on create-items comp

--- src/app/components/create-items/create-items.component.ts
+++ src/app/components/create-items/create-items.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-items',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-items.component.html',
   styleUrl: './create-items.component.scss'
 })
 export class CreateItemsComponent {
+
+  itemsForm!:FormGroup;
+    constructor(private _FormBuilder:FormBuilder){
+      this.itemsForm = this._FormBuilder.group({
+        name: ['',[Validators.required]],
+        codeItem: ['',[Validators.required]],
+        description: ['',[Validators.required]],
+        order: ['',[Validators.required]],
+        selectInp: ['',[Validators.required]],
+
+        purchasePrice: ['',[Validators.required]],
+        sellingPrice: ['',[Validators.required]],
+        wholesalePrice: ['',[Validators.required]],
+        semiWholesalePrice: ['',[Validators.required]],
+      });
+    }
+
+
+    submitItemsForm(){
+        if(this.itemsForm.valid){
+          console.log(this.itemsForm.value)
+
+        }else{
+          this.itemsForm.markAllAsTouched();
+        }
+    }
 
 }
